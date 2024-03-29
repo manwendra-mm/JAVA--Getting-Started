82f8047b43df39c52a421a2a612fcb110c6485b7
Manwendra Mahato <manwendramahato@gmail.com>
2024-03-29 13:03:12 +0530

    added two files

---
+++ Division.java
@@ -0,0 +1,11 @@
+public class Division {
+    public static void main (String[] arg){
+        //System.out.println(12/0);
+        //System.out.println(-12/0);
+        System.out.println(12/0.0);
+        System.out.println(-12/0.0);
+        System.out.println(0.0/0/0); // = NaN
+        System.out.println((0.0/0.0) == (0.0/0.0)); 
+    }
+    
+}
